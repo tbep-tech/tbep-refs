3cd078aa91ad1b529280a9ed6c491cf92d28899a
fawda123 <mbafs2012@gmail.com>
2019-12-12 08:09:23 -0500

    removing white space

--- R/02_nonascii.R
+++ R/02_nonascii.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(stringi)
 
 refs <- read.csv('tbep-refs.csv', stringsAsFactors = F) %>% 
-  mutate_if(is.character, stri_trans_general, id = 'latin-ascii')
+  mutate_all(as.character) %>% 
+  mutate_all(stri_trans_general, id = 'latin-ascii') %>% 
+  mutate_all(str_trim)
 
 write.csv(refs, 'tbep-refs.csv', row.names = F)
